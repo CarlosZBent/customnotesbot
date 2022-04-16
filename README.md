29a147ae3e9eba3749233004f7eadff634a97d26
Reinier Hernández <sasuke.reinier@gmail.com>
2022-04-16 10:41:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Custom Notes Bot
+Custon Notes for Telegram bot
+
+![Django CI](https://github.com/ragnarok22/customnotesbot/actions/workflows/django-test.yml/badge.svg)
+
+License
+-------
+
+Distributed under the terms of the [Apache License 2.0](LICENSE) license,
+
+Running in development mode
+------
+First install the dependencies
+
+    pip install -r requirements/develop.txt
+Then run the development server
+
+    python manage.py runserver --settings=config.settings.develop
---
+++ apps/bot/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+from apps.core.models import TimeStampedModel
+
+
+class UserBot(TimeStampedModel):
+    """
+    UserBot model
+    """
+    chat_id = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, blank=True, null=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    language_code = models.CharField(max_length=255, blank=True, null=True)
+
+    # Permissions
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        if self.username:
+            return self.username
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "UserBot"
+        verbose_name_plural = "UserBots"
+
+
+class Note(TimeStampedModel):
+    """
+    Note model for UserBot
+    """
+    user_bot = models.ForeignKey(UserBot, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Note"
+        verbose_name_plural = "Notes"
